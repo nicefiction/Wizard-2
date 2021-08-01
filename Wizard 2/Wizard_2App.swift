a09d9731bc6b484adb56225a15c992494fe20584
Olivier Van hamme <nicefiction@gmail.com>
2021-08-01 10:26:21 +0200

    Initial Commit

---
+++ Wizard 2/Wizard_2App.swift
@@ -0,0 +1,20 @@
+//
+//  Wizard_2App.swift
+//  Wizard 2
+//
+//  Created by Olivier Van hamme on 01/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct Wizard_2App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
